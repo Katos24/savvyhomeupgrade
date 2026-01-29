46fed61391789f1c69d76179b5a2d75660bc8d98
Alex Katos <alexkatos24@gmail.com>
2026-01-29 15:43:39 -0500

    fix: Disable prerendering for accept-invite page

--- app/accept-invite/page.tsx
+++ app/accept-invite/page.tsx
@@ -3,6 +3,9 @@
 import { useState, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
+export const dynamic = 'force-dynamic';
+
+
 export default function AcceptInvitePage() {
   const router = useRouter();
   const searchParams = useSearchParams();
