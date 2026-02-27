fe914fd46f04efa52ecafa952bfe34405dfa7575
Alex Katos <alexkatos24@gmail.com>
2026-02-27 16:13:41 -0500

    remove successRoute, add website link, new homepage

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -4,7 +4,6 @@ export default function UploadPage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 py-6 sm:py-12 px-4 sm:px-6 lg:px-8">
       <UploadForm 
-        successRoute="/success"
         headerTitle="Submit Your Project"
         headerSubtitle="Upload photos and get a fast, accurate assessment"
       />
